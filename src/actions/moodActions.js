4485b265400d1e30bf3a0cf3e6786410c8f8d07f
Joseph Tatum <josephtatum@gmail.com>
2020-02-13 13:36:54 -0800

    wrote action and tests for study(), takeNap(), and eatSnack(). all actions written and tests passing

--- src/actions/moodActions.js
+++ src/actions/moodActions.js
@@ -2,3 +2,19 @@ const DRINK_COFFEE = 'DRINK_COFFEE';
 export const drinkCoffee = () => ({
   type: DRINK_COFFEE
 });
+
+const EAT_SNACK = 'EAT_SNACK';
+export const eatSnack = () => ({
+  type: EAT_SNACK
+});
+
+const TAKE_NAP = 'TAKE_NAP';
+export const takeNap = () => ({
+  type: TAKE_NAP
+});
+
+const STUDY = 'STUDY';
+export const study = () => ({
+  type: STUDY
+});
+
--- src/actions/moodActions.test.js
+++ src/actions/moodActions.test.js
@@ -1,12 +1,37 @@
-import { drinkCoffee } from './moodActions';
+import { drinkCoffee, eatSnack, takeNap, study } from './moodActions';
 
 describe('moodActions', () => {
 
-  it('handles drinkingCoffee', () => {
-    
+  it('handles drinking coffee', () => {
+
     const action = drinkCoffee();
     expect(action).toEqual({
       type: 'DRINK_COFFEE'
     });
   });
+
+  it('handles eating snacks', () => {
+
+    const action = eatSnack();
+    expect(action).toEqual({
+      type: 'EAT_SNACK'
+    });
+  });
+
+  it('handles taking naps', () => {
+
+    const action = takeNap();
+    expect(action).toEqual({
+      type: 'TAKE_NAP'
+    });
+  });
+
+  it('handles studying', () => {
+
+    const action = study();
+    expect(action).toEqual({
+      type: 'STUDY'
+    });
+  });
+
 });
